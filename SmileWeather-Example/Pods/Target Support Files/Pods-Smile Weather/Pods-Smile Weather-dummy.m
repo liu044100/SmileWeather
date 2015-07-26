d77736d86430d138e0e81d5a08c9fcecb63a4172
rain_home <liu044100@gmail.com>
2015-07-26 21:40:35 +0900

    update cocoapods configure

---
+++ SmileWeather-Example/Pods/Target Support Files/Pods-Smile Weather/Pods-Smile Weather-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Smile_Weather : NSObject
+@end
+@implementation PodsDummy_Pods_Smile_Weather
+@end
--- SmileWeather-Example/Pods/Target Support Files/Pods-SmileWeather-Example-SmileWeather/Pods-SmileWeather-Example-SmileWeather-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SmileWeather_Example_SmileWeather : NSObject
-@end
-@implementation PodsDummy_Pods_SmileWeather_Example_SmileWeather
-@end
