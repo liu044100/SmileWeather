b700eb2c15574f5e6536beb9f5b85b4823a9a7e7
rain_home <liu044100@gmail.com>
2016-05-07 17:53:56 +0900

    refine watch UI

--- SmileWeather-Example/SmileWeatherWatch Extension/InterfaceController.m
+++ SmileWeather-Example/SmileWeatherWatch Extension/InterfaceController.m
@@ -13,6 +13,9 @@
 @property (unsafe_unretained, nonatomic) IBOutlet WKInterfaceLabel *weatherLabel;
 @property (unsafe_unretained, nonatomic) IBOutlet WKInterfaceLabel *tempLabel;
 @property (unsafe_unretained, nonatomic) IBOutlet WKInterfaceLabel *placeLabel;
+@property (unsafe_unretained, nonatomic) IBOutlet WKInterfaceLabel *conditionLabel;
+@property (unsafe_unretained, nonatomic) IBOutlet WKInterfaceLabel *maxTempLabel;
+@property (unsafe_unretained, nonatomic) IBOutlet WKInterfaceLabel *minTempLabel;
 
 @end
 
@@ -22,6 +25,8 @@
 - (void)awakeWithContext:(id)context {
     [super awakeWithContext:context];
     
+    [self resetLabelText];
+    
     CLLocation *location = [self getLocation];
     [[SmileWeatherDownLoader sharedDownloader] getWeatherDataFromLocation:location completion:^(SmileWeatherData *data, NSError *error) {
         if (error) {
@@ -30,9 +35,23 @@
             self.placeLabel.text = data.placeName;
             self.tempLabel.text = data.currentData.currentTempStri_Celsius;
             self.weatherLabel.text = data.currentData.icon;
+            self.conditionLabel.text = data.currentData.condition;
+            SmileWeatherForecastDayData *forecastData = data.forecastData.firstObject;
+            self.maxTempLabel.text = forecastData.highTempStri_Celsius;
+            self.minTempLabel.text = forecastData.lowTempStri_Celsius;
         }
     }];
 
+}
+
+-(void)resetLabelText {
+    NSString *blank = @"--";
+    self.placeLabel.text = blank;
+    self.conditionLabel.text = blank;
+    self.tempLabel.text = blank;
+    self.maxTempLabel.text = blank;
+    self.minTempLabel.text = blank;
+    self.weatherLabel.text = @"";
 }
 
 -(CLLocation*)getLocation{
